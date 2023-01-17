7b893440a64b249d1107fe034af2a3292780e099
raffal1993 <raffal1993@gmail.com>
2023-01-17 17:41:55 +0100

    home and categories and story added to routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from 'src/components/home/home.component';
+import { CategoryProductsComponent } from 'src/components/category-products/category-products.component';
+import { StoreProductsComponent } from 'src/components/store-products/store-products.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'categories/:categoryId', component: CategoryProductsComponent },
+  { path: 'stores/:storeId', component: StoreProductsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
