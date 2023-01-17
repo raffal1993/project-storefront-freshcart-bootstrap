1ee2c9106a65f49ed817e1c32c3cdc473165ffaf
raffal1993 <raffal1993@gmail.com>
2023-01-17 21:04:17 +0100

    feat: create store products component

---
+++ src/app/components/store-products/store-products.component-module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { StoreProductsComponent } from './store-products.component';
+
+@NgModule({
+  imports: [],
+  declarations: [StoreProductsComponent],
+  providers: [],
+  exports: [StoreProductsComponent]
+})
+export class StoreProductsComponentModule {
+}
---
+++ src/app/components/store-products/store-products.component.ts
@@ -0,0 +1,14 @@
+import {
+  ChangeDetectionStrategy,
+  Component,
+  ViewEncapsulation,
+} from '@angular/core';
+
+@Component({
+  selector: 'app-store-products',
+  styleUrls: ['./store-products.component.scss'],
+  templateUrl: './store-products.component.html',
+  encapsulation: ViewEncapsulation.Emulated,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class StoreProductsComponent {}
