2147958a68b45fc4092552fb16caa27f7beff057
raffal1993 <raffal1993@gmail.com>
2023-01-17 17:39:06 +0100

    feat: created store component

---
+++ src/components/store-products/store-products.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-store-products',
+  templateUrl: './store-products.component.html',
+  styleUrls: ['./store-products.component.scss']
+})
+export class StoreProductsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
