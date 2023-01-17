48a63c8c5a12a6fbc33d05ed8820a098113119de
raffal1993 <raffal1993@gmail.com>
2023-01-17 21:03:57 +0100

    feat: create home component

---
+++ src/app/components/home/home.component.ts
@@ -0,0 +1,14 @@
+import {
+  ChangeDetectionStrategy,
+  Component,
+  ViewEncapsulation,
+} from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  styleUrls: ['./home.component.scss'],
+  templateUrl: './home.component.html',
+  encapsulation: ViewEncapsulation.Emulated,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class HomeComponent {}
