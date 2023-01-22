ba6d153589941d12d6f8f2e659389d6182cb6b07
raffal1993 <raffal1993@gmail.com>
2023-01-22 22:29:40 +0100

    feat: add sort panel to products-content component

--- src/app/components/products-content/products-content.component-module.ts
+++ src/app/components/products-content/products-content.component-module.ts
@@ -1,9 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { ProductsContentSortingComponentModule } from '../products-content-sorting/products-content-sorting.component-module';
 import { ProductsContentComponent } from './products-content.component';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, ProductsContentSortingComponentModule],
   declarations: [ProductsContentComponent],
   providers: [],
   exports: [ProductsContentComponent],
