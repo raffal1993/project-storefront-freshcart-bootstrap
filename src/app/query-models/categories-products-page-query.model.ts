1705364ae4fb87704d56850bb8970849acbf7731
raffal1993 <raffal1993@gmail.com>
2023-01-22 17:33:49 +0100

    create ProductQueryModel

--- src/app/query-models/categories-products-page-query.model.ts
+++ src/app/query-models/categories-products-page-query.model.ts
@@ -3,3 +3,22 @@ export interface CategoryQueryModel {
   imageUrl: string;
   id: string;
 }
+
+export interface ProductQueryModel {
+  name: string;
+  price: number;
+  categoryId: string;
+  ratingValue: number;
+  ratingCount: number;
+  ratingStars: RatingStars;
+  imageUrl: string;
+  featureValue: number;
+  storeIds: string[];
+  id: string;
+}
+
+interface RatingStars {
+  fill: any[];
+  half: any[];
+  empty: any[];
+}
