924ff599883f83c26fbde06fbf298aa4e9752878
raffal1993 <raffal1993@gmail.com>
2023-01-22 22:28:05 +0100

    create sortOptions variable

---
+++ src/app/commons/sortOptions.ts
@@ -0,0 +1,8 @@
+import { SortOptions } from '../types/sortOptions.type';
+
+export const sortOptions: SortOptions[] = [
+  'Featured',
+  'Price: Low to High',
+  'Price: High to Low',
+  'Avg. Rating',
+];
