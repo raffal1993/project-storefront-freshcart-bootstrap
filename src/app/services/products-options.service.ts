042a8d4e3e78510b3e7ca6466e240f10f59e1b40
raffal1993 <raffal1993@gmail.com>
2023-01-23 21:26:47 +0100

    create price filter subject

--- src/app/services/products-options.service.ts
+++ src/app/services/products-options.service.ts
@@ -2,9 +2,13 @@ import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 import { sortOptions } from '../commons/sortOptions';
 import { SortOptions } from '../types/sortOptions.type';
+import { PriceFilter } from '../types/priceFilter.type';
 
 @Injectable({ providedIn: 'root' })
 export class ProductsOptionsService {
   readonly _activeSortOptionsSubject: BehaviorSubject<SortOptions> =
     new BehaviorSubject<SortOptions>(sortOptions[0]);
+
+  readonly _priceFilterSubject: BehaviorSubject<PriceFilter> =
+    new BehaviorSubject<PriceFilter>({ priceFrom: null, priceTo: null });
 }
