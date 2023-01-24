83d95bda155a03f05d4edc7fa508eb3c5ed45a83
raffal1993 <raffal1993@gmail.com>
2023-01-24 12:14:07 +0100

    feat: create & add pagination

--- src/app/components/products-content/products-content.component.ts
+++ src/app/components/products-content/products-content.component.ts
@@ -2,12 +2,28 @@ import {
   ChangeDetectionStrategy,
   Component,
   Input,
+  SimpleChange,
   ViewEncapsulation,
 } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import {
+  BehaviorSubject,
+  Observable,
+  combineLatest,
+  map,
+  of,
+  shareReplay,
+} from 'rxjs';
 import {
   CategoryQueryModel,
   ProductQueryModel,
-} from 'src/app/query-models/categories-products-page-query.model';
+} from '../../query-models/categories-products-page-query.model';
+import { PaginationOptions } from '../../types/paginationOptions.type';
+import { ProductsOptionsService } from '../../services/products-options.service';
+import {
+  defaultPageOptions,
+  pageLimitOptions,
+} from 'src/app/commons/paginationOptions';
 
 @Component({
   selector: 'app-products-content',
@@ -19,4 +35,81 @@ import {
 export class ProductsContentComponent {
   @Input() category: CategoryQueryModel | null = null;
   @Input() products: ProductQueryModel[] | null = [];
+
+  private _productsDataSubject: BehaviorSubject<ProductQueryModel[]> =
+    new BehaviorSubject<ProductQueryModel[]>([]);
+
+  readonly productsData$: Observable<ProductQueryModel[]> =
+    this._productsDataSubject.asObservable().pipe(shareReplay(1));
+
+  readonly pageOptions$: Observable<PaginationOptions> =
+    this._productsOptionsService._paginationOptionsSubject
+      .asObservable()
+      .pipe(shareReplay(1));
+
+  readonly productsToDisplay$: Observable<ProductQueryModel[]> = combineLatest([
+    this.productsData$,
+    this.pageOptions$,
+  ]).pipe(
+    map(
+      ([products, { limit, pagination }]) =>
+        products &&
+        products.slice(
+          (pagination - 1) * limit,
+          (pagination - 1) * limit + limit
+        )
+    )
+  );
+
+  readonly pages$: Observable<number[]> = combineLatest([
+    this.productsData$,
+    this.pageOptions$,
+  ]).pipe(
+    map(([products, options]) => {
+      const pages = products ? Math.ceil(products.length / options.limit) : 0;
+      return [...Array(pages + 1).keys()].slice(1);
+    })
+  );
+
+  readonly pageLimit$ = of(pageLimitOptions);
+
+  readonly paginationForm: FormGroup = new FormGroup({
+    limit: new FormControl(defaultPageOptions.limit),
+    pagination: new FormControl(defaultPageOptions.pagination),
+  });
+
+  constructor(private _productsOptionsService: ProductsOptionsService) {}
+
+  onPageOptionsChange(value: number, name: keyof PaginationOptions): void {
+    this.paginationForm.get(name)?.setValue(value);
+  }
+
+  ngAfterViewInit(): void {
+    combineLatest([
+      this.productsData$,
+      this.paginationForm.valueChanges,
+    ]).subscribe(([products, values]) => {
+      const hasToChangePage =
+        products.length <= values.pagination * values.limit - values.limit;
+
+      const newPage = Math.ceil(products.length / values.limit);
+
+      hasToChangePage &&
+        this.paginationForm.get('pagination')?.setValue(newPage);
+
+      const newValues = hasToChangePage
+        ? { ...values, pagination: newPage }
+        : values;
+
+      return this._productsOptionsService._paginationOptionsSubject.next({
+        ...newValues,
+      });
+    });
+  }
+
+  ngOnChanges(changes: { [key: string]: SimpleChange }) {
+    if (changes.hasOwnProperty('products')) {
+      this._productsDataSubject.next(changes['products'].currentValue);
+    }
+  }
 }
